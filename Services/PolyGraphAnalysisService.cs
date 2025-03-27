5b5740ca31cadfcd3045f2a2af71c9797babd7ca
CadeXLegend <foundernq@gmail.com>
2025-03-27 22:50:27 +1000

    fix: refactored, squashed the 'and' bug

--- Services/PolyGraphAnalysisService.cs
+++ Services/PolyGraphAnalysisService.cs
@@ -12,98 +12,87 @@ public class PolyGraphAnalysisService : IPolyGraphAnalysisService
 
     public string ToWords(IPolyGraph<TriadicNumber> Graph)
     {
-        var result = new StringBuilder();
+        List<string> sentence = [];
         var polygons = Graph.Polygons();
         var scale = GetLongScale(Graph);
-        bool needsAnd = true;
 
         int polygonLength = polygons.Length;
         for (int i = polygonLength - 1; i >= 0; --i)
         {
-            var nodes = polygons[i].Nodes();
-            string polygonWords = polygonLength is 1 && nodes.Length is 1 && nodes[0].Entity().Number is 0 ?
-                $"{(Digits)nodes[0].Entity().Number}" : PolygonToWords(polygons[i]);
-
-            result.Append(polygonWords);
-
-            bool areCurrentNodesZero = nodes.Length is not 3 && nodes.All(n => n.Entity().Number is 0);
-            if (areCurrentNodesZero)
-                continue;
-
             bool isLongScaleGroup = scale is not LongScales.Hundred && i > 0;
-            LongScales currentLongScale = (LongScales)i + 1;
-            if (isLongScaleGroup)
-            {
-                needsAnd = true;
-                result.Append(' ').Append(currentLongScale.ToString());
-            }
+            string longScale = isLongScaleGroup ? $" {(LongScales)i + 1}" : string.Empty;
+            var nodes = polygons[i].Nodes();
 
-            if (needsAnd && i < 1 &&
-                polygonLength > 1 &&
-                currentLongScale is not LongScales.Hundred)
-            {
-                result.Append(" And ");
-                needsAnd = false;
-            }
-            else
-            {
-                if (i > 0)
-                    result.Append(' ');
+            // if the only number is 0, then the word is Zero, ez, get out
+            // otherwise, let's transform the polygons into words
+            string polygonWords = polygonLength is 1 && nodes.Length is 1 && nodes[0].Entity().Number is 0 ?
+                $"{(Digits)nodes[0].Entity().Number}" : PolygonToWords(polygons[i], longScale);
 
-                needsAnd = true;
-            }
+            // We only wanna add the And delimiter if the last
+            // triadic number group isn't all zeros (0, 00, or 000)
+            // and isn't a part of a long scale group
+            // and the hundreds position number is Zero
+            // meaning it's either a ten, teen, or digit
+            bool isLastNodeZero = nodes.Length == 3 && nodes[2].Entity().Number == 0;
+            bool allNodesAreNotZero = nodes.Select(n => n.Entity().Number).Sum() > 0;
+            string finalDelimiter = allNodesAreNotZero && !isLongScaleGroup && isLastNodeZero ? "And " : string.Empty;
+            
+            sentence.Add($"{finalDelimiter}{polygonWords}");
         }
 
-        return result.ToString().Trim();
+        return string.Join(' ', sentence);
     }
 
-    private static string PolygonToWords(IPolygon<TriadicNumber> polygon)
+    private static string PolygonToWords(IPolygon<TriadicNumber> polygon, string longScale)
     {
         var nodes = polygon.Nodes();
         return nodes.Length switch
         {
-            1 => SingleDigitToWords((Node)nodes[0]),
-            2 => TwoDigitsToWords((Node)nodes[0], (Node)nodes[1]),
-            3 => ThreeDigitsToWords((Node)nodes[0], (Node)nodes[1], (Node)nodes[2]),
+            1 => SingleDigitToWords((Node)nodes[0], longScale),
+            2 => TwoDigitsToWords((Node)nodes[0], (Node)nodes[1], longScale),
+            3 => ThreeDigitsToWords((Node)nodes[0], (Node)nodes[1], (Node)nodes[2], longScale),
             _ => default,
         };
     }
 
-    private static string SingleDigitToWords(Node node)
+    private static string SingleDigitToWords(Node node, string longScale)
     {
-        return node.Number is 0 ? string.Empty : $"{(Digits)node.Number}";
+
+        return node.Number is 0 ? string.Empty : $"{(Digits)node.Number}{longScale}";
     }
 
-    private static string TwoDigitsToWords(Node ones, Node tens)
+    private static string TwoDigitsToWords(Node ones, Node tens, string longScale)
     {
         int number = (tens.Number * 10) + ones.Number;
         if (number is 0)
             return string.Empty;
         if (number < 9)
-            return SingleDigitToWords(ones);
+            return SingleDigitToWords(ones, longScale);
         if (number < 20)
-            return $"{(Teens)number}";
+            return $"{(Teens)number}{longScale}";
         if (ones.Number is 0)
             return $"{(Tens)tens.Number}";
-        return $"{(Tens)tens.Number}-{(Digits)ones.Number}";
+        return $"{(Tens)tens.Number}-{(Digits)ones.Number}{longScale}";
     }
 
-    private static string ThreeDigitsToWords(Node ones, Node tens, Node hundreds)
+    private static string ThreeDigitsToWords(Node ones, Node tens, Node hundreds, string longScale)
     {
         if (hundreds.Number + tens.Number + ones.Number is 0)
             return string.Empty;
 
         var result = new StringBuilder();
         if (hundreds.Number > 0)
-            result.Append($"{(Digits)hundreds.Number} Hundred");
+            result.Append($"{(Digits)hundreds.Number} {ShortScales.Hundred}");
 
-        string twoDigits = TwoDigitsToWords(ones, tens);
+        string twoDigits = TwoDigitsToWords(ones, tens, longScale);
         if (!string.IsNullOrEmpty(twoDigits))
         {
             if (result.Length > 0)
                 result.Append(" And ");
             result.Append(twoDigits);
         }
+
+        result.Append(longScale);
 
         return result.ToString();
     }
